4c0a39b652eb348ea75c9f265ade5943870b85b5
kathy-nevola <72228735+kathy-nevola@users.noreply.github.com>
2021-06-02 14:11:58 -0400

    updates to red_cat

--- R/red_cat.R
+++ R/red_cat.R
@@ -3,14 +3,13 @@
 #' @param df a dataframe of cat names and colors
 #' @return The list of names of red cats.
 #' @importFrom magrittr %>%
-#' @import dplyr filter
-#' @export red
+#' @importFrom  dplyr filter
 #' @example
-#' df <- data.frame(Name = c("Abe", "Bob", "Charlie"),
-#'                  Color = c("brown", "red", "gray"))
-#' red_cat(df)
+#' red_cat(data.frame(Name = c("Abe", "Bob", "Charlie"), Color = c("brown", "red", "gray")))
 
 red_cat <- function(df){
+  names(df) <- c("Name", "Color")
   rc <- df %>% filter(Color == "red")
   red <- rc$Name
+  return(red)
 }
