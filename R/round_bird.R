f882840492d4b2236f02c3085721ea8babf25d72
kathy-nevola <72228735+kathy-nevola@users.noreply.github.com>
2021-06-02 14:36:28 -0400

    created round bird function

---
+++ R/round_bird.R
@@ -0,0 +1,15 @@
+#' Tells you which birds are round
+#'
+#' @param df a dataframe of cat names and colors
+#' @return The list of names of red cats.
+#' @importFrom magrittr %>%
+#' @importFrom  dplyr filter
+#' @example
+#' round_bird(data.frame(Name = c("Bearded reedling", "Egret", "Sparrow"), Shape = c("round", "tall", "average")))
+
+round_bird <- function(df){
+  names(df) <- c("Name", "Shape")
+  rb <- df %>% filter(Shape == "round")
+  round <- rb$Name
+  return(round)
+}
