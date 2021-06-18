a5686d182d043b81da82bf079404eb96c3293878
kathy-nevola <72228735+kathy-nevola@users.noreply.github.com>
2021-06-18 12:10:26 -0400

    styler fixes

--- R/round_bird.R
+++ R/round_bird.R
@@ -5,9 +5,11 @@
 #' @importFrom magrittr %>%
 #' @importFrom  dplyr filter
 #' @example
-#' round_bird(data.frame(Name = c("Bearded reedling", "Egret", "Sparrow"), Shape = c("round", "tall", "average")))
+#' round_bird(data.frame(Name = c("Bearded reedling",
+#'                                 "Egret", "Sparrow"),
+#'                       Shape = c("round", "tall", "average")))
 
-round_bird <- function(df){
+round_bird <- function(df) {
   names(df) <- c("Name", "Shape")
   rb <- df %>% filter(Shape == "round")
   round <- rb$Name
